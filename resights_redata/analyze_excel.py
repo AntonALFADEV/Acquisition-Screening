d5ec76cd7c73d1922d35c87005b5dbe43222a8f5
AntonALFADEV <anton@alfadev.dk>
2025-08-07 13:10:17 +0200

    Add files via upload

--- resights_redata/analyze_excel.py
+++ resights_redata/analyze_excel.py
@@ -1,1 +1,6 @@
-analyse excel
+import pandas as pd
+
+def analyze_excel(file):
+    # Enkel preview af uploadet Excel-fil
+    df = pd.read_excel(file)
+    return df.head(50)  # viser de første 50 rækker
